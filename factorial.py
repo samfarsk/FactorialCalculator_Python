24279d4ee0a8661e9a4005c5e02ad48bb142a863
Samuel Faria Schadeck <89159750+samfasch@users.noreply.github.com>
2024-05-23 19:29:47 -0300

    Changed variable.

--- factorial.py
+++ factorial.py
@@ -1,15 +1,15 @@
 while True:
     # Define a entrada
-    valor = int(input("Digite um número inteiro: "))
+    valorInput = int(input("Digite um número inteiro: "))
 
     # Cria os contadores
     fatorial = 1
-    numero = 1
+    contagem = 1
 
     # Cálculo do fatorial
-    while numero <= valor:
-        fatorial *= numero
-        numero += 1
+    while contagem <= valorInput:
+        fatorial *= contagem
+        contagem += 1
 
     # Retorna o fatorial
-    print(f"\nO fatorial de {valor} é {fatorial}.")
+    print(f"\nO fatorial de {valorInput} é {fatorial}.\n")
