02769b8940652b1badee385c883482369e8512ac
Samuel Faria Schadeck <89159750+samfasch@users.noreply.github.com>
2024-05-23 18:04:32 -0300

    While added

--- factorial.py
+++ factorial.py
@@ -1,14 +1,15 @@
-# Define a entrada
-valor = int(input("Digite um número: "))
+while True:
+    # Define a entrada
+    valor = int(input("Digite um número: "))
 
-# Cria os contadores
-fatorial = 1
-numero = 1
+    # Cria os contadores
+    fatorial = 1
+    numero = 1
 
-# Cálculo do fatorial
-while numero <= valor:
-    fatorial *= numero
-    numero += 1
+    # Cálculo do fatorial
+    while numero <= valor:
+        fatorial *= numero
+        numero += 1
 
-# Retorna o fatorial
-print(fatorial)
+    # Retorna o fatorial
+    print(fatorial)
